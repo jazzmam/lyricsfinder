785f6c07827025022fa7d5bde0306b0ed04b5876
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-01-06 15:29:53 +0200

    .

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("works");
+const form = document.getElementById('form');
+const saerch = document.getElementById('saerch');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiUrl = 'https://api.lyrics.ovh';
