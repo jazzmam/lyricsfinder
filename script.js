c05c0cecc95be2fb8c079baf25d3f4ae649f337a
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-01-06 18:18:37 +0200

    .

--- script.js
+++ script.js
@@ -3,4 +3,57 @@ const saerch = document.getElementById('saerch');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
 
-const apiUrl = 'https://api.lyrics.ovh';
+const apiUrl = 'https://api.lyrics.ovh/suggest';
+
+async function searchSongs(term) {
+	const res = await fetch(`${apiUrl}/${term}`);
+	const data = await res.json();
+
+	showData(data);
+}
+
+// Show songs / artists in DOM
+function showData(data) {
+
+	result.innerHTML = `
+		<ul class="songs">
+			${data.data.map(song => `
+			<li>
+				<span><strong>${song.artist.name}</strong> - ${song.title}</span>
+				<button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Get Lyrics</button>
+			</li>
+		`). join('')
+	}
+		</ul>
+	`;
+
+	if (data.prev || data.next) {
+		more.innerHTML = `
+			${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Previous</button>` : ``}
+			${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ``}
+		`;
+	} else {
+		more.innerHTML = ``;
+	}
+}
+
+async function getMoreSongs(url) {
+	const res = await fetch(url);
+	const data = await res.json();
+	
+	showData(data);
+}	
+
+// Event listeners
+form.addEventListener('submit', e => {
+	// not allow to submit a form to a file
+	e.preventDefault();
+
+	const searchWord = search.value.trim();
+
+	if (!searchWord) {
+		alert('Please type in a word');
+	} else {
+		searchSongs(searchWord);
+	}
+});
